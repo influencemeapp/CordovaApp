d3d424851cd2c163170288f140b2cf4fc850531b
Anil Yadav <anilkumar.yadav@rootinfosol.com>
2018-04-27 18:57:28 +0530

    ios files added

---
+++ platforms/ios/www/public/services/serviceParam.js
@@ -0,0 +1,135 @@
+'use strict';
+
+app.service('paramAthlete', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(id) {
+			stringValue = id;
+		}
+	};
+});
+app.service('paramPlan', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(id) {
+			stringValue = id;
+		}
+	};
+});
+app.service('paramRoutine', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(id) {
+			stringValue = id;
+		}
+	};
+});
+app.service('paramSearch', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(value) {
+			//console.log(value)
+			stringValue = value;
+		}
+	};
+});
+app.service('paramsStartRoutine', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(data) {
+			stringValue = data;
+		}
+	};
+});
+app.service('paramsCustomerPlan', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(data) {
+			stringValue = data;
+		}
+	};
+});
+/* app.service('paramisNewUser', function () {
+	var stringValue = '';
+	return {
+		getString: function () {
+			return stringValue;
+		},
+		setString: function (data) {
+			stringValue = data;
+		}
+	}
+});
+app.service('paramType', function () {
+	var stringValue = '';
+	return {
+		getString: function () {
+			return stringValue;
+		},
+		setString: function (data) {
+			stringValue = data;
+		}
+	}
+}); */
+app.service('paramImgDay', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(data) {
+			stringValue = data;
+		}
+	};
+});
+app.service('paramStartDaily', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(data) {
+			stringValue = data;
+		}
+	};
+});
+app.service('typeLoginParam', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(data) {
+			stringValue = data;
+		}
+	};
+});
+app.service('goPlans', function () {
+	var stringValue = '';
+	return {
+		getString: function getString() {
+			return stringValue;
+		},
+		setString: function setString(data) {
+			stringValue = data;
+		}
+	};
+});
